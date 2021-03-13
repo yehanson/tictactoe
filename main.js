a4d3ce4f7618017bea4b8f804e282b94dd86f73d
fyeguop <80563592+fyeguop@users.noreply.github.com>
2021-03-13 00:20:00 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+
+function changeBlue1(){
+    document.getElementById('box1').src='Images/blue.jpg';
+};
+function changeRed1(){
+    document.getElementById('box1').src='Images/red.jpg';
+};
+function changeBlue2(){
+    document.getElementById('box2').src='Images/blue.jpg';
+};
+function changeRed2(){
+    document.getElementById('box2').src='Images/red.jpg';
+};
+function changeBlue3(){
+    document.getElementById('box3').src='Images/blue.jpg';
+};
+function changeRed3(){
+    document.getElementById('box3').src='Images/red.jpg';
+};
+function changeBlue4(){
+    document.getElementById('box4').src='Images/blue.jpg';
+};
+function changeRed4(){
+    document.getElementById('box4').src='Images/red.jpg';
+};
+function changeBlue5(){
+    document.getElementById('box5').src='Images/blue.jpg';
+};
+function changeRed5(){
+    document.getElementById('box5').src='Images/red.jpg';
+};
+function changeBlue6(){
+    document.getElementById('box6').src='Images/blue.jpg';
+};
+function changeRed6(){
+    document.getElementById('box6').src='Images/red.jpg';
+};
+function changeBlue7(){
+    document.getElementById('box7').src='Images/blue.jpg';
+};
+function changeRed7(){
+    document.getElementById('box7').src='Images/red.jpg';
+};
+function changeBlue8(){
+    document.getElementById('box8').src='Images/blue.jpg';
+};
+function changeRed8(){
+    document.getElementById('box8').src='Images/red.jpg';
+};
+function changeBlue9(){
+    document.getElementById('box9').src='Images/blue.jpg';
+};
+function changeRed9(){
+    document.getElementById('box9').src='Images/red.jpg';
+};
